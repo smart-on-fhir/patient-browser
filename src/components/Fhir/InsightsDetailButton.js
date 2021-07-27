1c25341169f77c4eacbb0043d134cf3cb8656251
kathrynelrod <elrodkathryn@gmail.com>
2021-07-27 14:01:08 -0400

    Condensed InsightsPopup into InsightsDetailButton

--- src/components/Fhir/InsightsDetailButton.js
+++ src/components/Fhir/InsightsDetailButton.js
@@ -1,7 +1,10 @@
 import React         from "react"
 import PropTypes     from "prop-types"
 import Popup         from "reactjs-popup"
-import InsightsPopup from "../InsightsPopup"
+import { 
+    getInsightDetails,
+    InsightSource
+} from "../../lib"
 
 export default class InsightsDetailButton extends React.Component {
     static PropTypes = {
@@ -9,6 +12,9 @@ export default class InsightsDetailButton extends React.Component {
     }
 
     render() {
+        let rec = this.props.resource
+        let deets = getInsightDetails(rec)
+        let isDocument = deets.insightSource==InsightSource.DOCUMENT
         return (
             <div style={{ color: '#337ab7', textAlign: 'center' }}>
                 <Popup
@@ -20,7 +26,47 @@ export default class InsightsDetailButton extends React.Component {
                     position="left center"
                     on={['hover', 'click', "focus"]}
                 >
-                    <InsightsPopup resource={this.props.resource}/>
+                    <div style={{boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} className="panel panel-default">
+                        <div className="panel-heading">
+                            <b className="text-primary">
+                                <i className="fa fa-lightbulb-o fas fa-bold" style={{paddingRight: 10}}/>Insights for {rec.resourceType} {rec.id}
+                            </b>
+                        </div>
+                        <div className="table-responsive">
+                            <table className="table table-condensed table-hover table-striped table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <td style={{fontWeight: "bold"}}>Process Name</td>
+                                        <td>{deets.processName}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{fontWeight: "bold"}}>Process Type</td>
+                                        <td>{deets.processType}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{fontWeight: "bold"}}>Process Version</td>
+                                        <td>{deets.processVersion}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{fontWeight: "bold"}}>Insight Source</td>
+                                        <td>{isDocument ? deets.basedOn.replace("/", " ") : deets.insightSource}</td>
+                                    </tr>
+                                </tbody>
+                                { isDocument ?
+                                <tbody>
+                                    <tr>
+                                        <td style={{fontWeight: "bold"}}>Covered Text</td>
+                                        <td><em>"{deets.coveredText}"</em></td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{fontWeight: "bold"}}>Confidence</td>
+                                        <td>{Number(deets.confidence)*100}%</td>
+                                    </tr>
+                                </tbody>
+                                : null }
+                            </table>
+                        </div>
+                    </div>
                 </Popup>
             </div>
         )
