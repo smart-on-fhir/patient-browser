61302473ef689014a1108e89eaf1ae49c168f018
Dylan Phelan <Dtphelan1@gmail.com>
2023-10-10 15:34:44 -0400

    remove logs; point serve at correct directory

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,8 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
-  console.log(env.NODE_ENV)
   const isProd = env.NODE_ENV === "production" 
 
-  console.log({
-    plugins: [react(), splitVendorChunkPlugin()],
-    ...(isProd && {build: {sourcemap: true}})
-  })
   return {
     plugins: [react(), splitVendorChunkPlugin()],
     ...(isProd && {build: {sourcemap: true}})
