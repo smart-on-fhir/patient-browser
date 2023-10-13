d75620e0ff20d30800e340599dc0de69083c7d0f
Dylan Phelan <Dtphelan1@gmail.com>
2023-10-13 17:41:10 -0400

    remove bootstrap dependency; move bootstrap code to assets dir

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import ReactDOM from "react-dom/client";
-import * as bootstrap from "bootstrap";
 import { Provider } from "react-redux";
 import { Route, Routes } from "react-router";
 import { HashRouter } from "react-router-dom";
 import STORE from "./redux";
-// Import early so other children component styles cascade over bootstrap styles
-import "./main.scss";
 import App from "./components/App";
 import PatientList from "./components/PatientList";
 import PatientDetail from "./components/PatientDetail";
@@ -24,8 +21,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </App>
   </Provider>
 );
-
-const body = document.getElementsByTagName("body")[0];
-new bootstrap.Tooltip(body, {
-  selector: ".patient-detail-page [title]",
-});
